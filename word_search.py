8d8cef970f26f70fa4deaea88628fe55b472b486
khekrn <kishore.karunakaran@digitap.ai>
2023-11-11 20:36:32 +0530

    word_search_solution

--- word_search.py
+++ word_search.py
@@ -1,34 +1,55 @@
-#board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-#word = 'ABCCED'
-#board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-#word = "ABCB"
 board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "SEE"
-
-m = 3
-n = 4
+#word = 'SEE'
+#word = 'ABCCED'
+word = 'ABCB'
+#board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+#board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+#word = "ABCS"
+#board = [["A","Z"],["C","E"]]
+#word = "CA"
+m = len(board)
+n = len(board[0])
 rows, cols = (m, n)
 track = [[True for i in range(cols)] for j in range(rows)]
-print(track)
 ans = ""
 def search(board,track,r,c,p,word,ans):
-    if p == word:
-        ans = True
-        print(ans)
-        track[r][c] = False
-        return
     if not (track[r][c]):
-        return1
+        return
+    if len(p)==len(word):
+        if p==word:
+            ans = True
+        track[r][c] = True
+        return ans
     track[r][c] = False
     if r<len(board)-1:
-        search(board,track,r+1,c,p+board[r][c],word,ans)
+        if(search(board,track,r+1,c,p+board[r+1][c],word,ans)):
+           return True
     if c<len(board[0])-1:
-        search(board, track, r, c+1, p + board[r][c], word,ans)
+        if(search(board, track, r, c+1, p+board[r][c+1], word,ans)):
+            return True
     if r>0:
-        search(board, track, r-1, c, p + board[r][c], word,ans)
+        if(search(board, track, r-1, c, p+board[r-1][c], word,ans)):
+            return True
     if c>0:
-        search(board, track, r, c-1, p + board[r][c], word,ans)
+        if(search(board, track, r, c-1, p+board[r][c-1], word,ans)):
+            return True
     track[r][c] = True
-search(board,track,0,0,"",word,ans)
-print(ans)
+    return
+def findtheword(board,r,c,word):
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] == word[0]:
+                ans = search(board,track,i,j,str(board[i][j]),word,False)
+                if(ans):
+                    return True
+    return False
+
+    #            if ans==True:
+    #                return True
+    #return False
+
+print(findtheword(board,rows,cols,word))
+
+#print(search(board,track,1,3,"",word,ans))
+#print(ans)
 
